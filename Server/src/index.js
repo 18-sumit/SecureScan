896f6d450fc08a8da75d4e3396b8c7f5fb8fc897
Sumit Singh <sumitsingh3076@gmail.com>
2025-01-28 23:46:03 +0530

    startup logic

---
+++ Server/src/index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDB from "../src/config/db.js"
+dotenv.config({
+    path: "/.env"
+})
+
+try {
+    await connectDB();  // Wait for the DB connection to succeed
+    app.on("error", (error) => {
+        console.log("ERRR: ", error);
+        throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at Port : ${process.env.PORT}`);
+    });
+} catch (err) {
+    console.log("MONGODB CONNECTION FAILED !!", err);
+}
