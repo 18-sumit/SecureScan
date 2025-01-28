0206915f3aac3acb3870d4fc088f0a13f340e6f7
Sumit Singh <sumitsingh3076@gmail.com>
2025-01-28 22:46:01 +0530

    databases connection with mongoDB

---
+++ Server/src/config/db.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../src/utils/constants.js";
+import dotenv from "dotenv"
+
+// Load environment variables from .env file
+dotenv.config()
+
+const connectDB = async () => {
+
+    try {
+
+        const mongoURI = `${process.env.MONGODB_URI}/${DB_NAME}`;
+
+        const connectionInstance = await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log(`\n MONGODB connected with name : ${connectionInstance.connect.name}`)
+    } catch (error) {
+        console.log(`MONGODB connection failed due to ${error.message}`)
+        process.exit(1);
+    }
+}
+
+export default connectDB;
