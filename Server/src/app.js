320ebd9292a22aa15f409f8e34d9a3f80fd7b09c
Sumit Singh <sumitsingh3076@gmail.com>
2025-02-03 21:14:07 +0530

    MORGAN setup

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,8 +1,13 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import logger from "./logger";
+import morgan from "morgan";
 
 const app = express();
+
+
+
 
 app.use(cors(
     {
@@ -22,6 +27,26 @@ app.use(express.urlencoded({
 }))
 
 app.use(cookieParser())
+
+// MORGAN:
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+    morgan(morganFormat, {
+        stream: {
+            write: (message) => {
+                const logObject = {
+                    method: message.split(" ")[0],
+                    url: message.split(" ")[1],
+                    status: message.split(" ")[2],
+                    responseTime: message.split(" ")[3],
+                };
+                logger.info(JSON.stringify(logObject));
+            },
+        },
+    })
+);
 
 
 
