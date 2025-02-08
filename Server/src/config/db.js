88750e75eaa973182fb42d4e6e6d8b1430e560d4
Sumit Singh <sumitsingh3076@gmail.com>
2025-02-08 19:36:52 +0530

    bug fixed

--- Server/src/config/db.js
+++ Server/src/config/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../utils/constants.js";
 import dotenv from "dotenv"
+import logger from "../logger.js";
 
 // Load environment variables from .env file
 dotenv.config()
@@ -13,9 +14,9 @@ const connectDB = async () => {
 
         const connectionInstance = await mongoose.connect(mongoURI);
 
-        console.log(`\n MONGODB connected with name : ${connectionInstance.connect.name}`)
+        logger.info(`\n MONGODB connected with HOST : ${connectionInstance.connection.host}`)
     } catch (error) {
-        console.log(`MONGODB connection failed due to ${error.message}`)
+        logger.error(`MONGODB connection failed due to ${error.message}`)
         process.exit(1);
     }
 }
