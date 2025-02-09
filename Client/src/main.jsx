a7f872b9f08585fd6a4b6b7cd57e8a431804f99b
Sumit Singh <sumitsingh3076@gmail.com>
2025-02-10 00:26:53 +0530

    clerk setup in main

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -4,17 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react';
 
-const CLERK_PUBLISHABLE_KEY = import.meta.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+const CLERK_PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!CLERK_PUBLISHABLE_KEY) {
-  throw new Error("NO PUBLISH KEY FOUND");
-
+  throw new Error("Missing Publishable Key")
 }
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ClerkProvider publishKey={CLERK_PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <App />
+    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY} afterSignOutUrl="/">
+    <App />
     </ClerkProvider>
   </StrictMode>,
 )
