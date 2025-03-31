be9cedca0e745f9f8ed10acc3cfbd58001aefc01
Sumit Singh <sumitsingh3076@gmail.com>
2025-03-31 11:50:14 +0530

    updated

---
+++ Server/src/middleware/auth.middleware.js
@@ -0,0 +1,33 @@
+import { ApiError } from "../utils/ApiError.utils.js";
+import { asyncHandler } from "../utils/asyncHandler.utils.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.models.js";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.
+            replace("Bearer", "")
+
+        // no token available , in case of mobile app
+        if (!token) {
+            new ApiError(401, "Unauthorized Request!");
+        }
+
+        // if token available , then decode it 
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+
+        // if we do not get a user 
+        if (!user) {
+            throw new ApiError(401, "Invalid Access Token");
+        }
+
+        req.user = user;
+        // as we know from cookieParser we can get the refrence of user to req.user
+
+
+        next()
+    } catch (error) {
+        throw new ApiError(401, error.message || "Invalid access token ")
+    }
+})
--- Server/src/routes/user.routes.js
+++ Server/src/routes/user.routes.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
-import { registerUser, loginUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
 
 
 const userRouter = Router();
 
 userRouter.route('/register').post(registerUser);
 userRouter.route('/login').post(loginUser);
+
+// protected route:
+userRouter.route('/logout').post(verifyJWT, logoutUser)
 
 
 
