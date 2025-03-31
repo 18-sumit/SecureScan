931c6d81f90b59b35d5edb7dcd75d9044850ff34
Sumit Singh <sumitsingh3076@gmail.com>
2025-03-31 12:30:38 +0530

    updated

--- Server/src/routes/user.routes.js
+++ Server/src/routes/user.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
+import { rateLimiter } from "../middleware/rateLimiter.middleware.js";
 
 
 const userRouter = Router();
 
-userRouter.route('/register').post(registerUser);
-userRouter.route('/login').post(loginUser);
+userRouter.route('/register').post(rateLimiter ,registerUser);
+userRouter.route('/login').post(rateLimiter ,loginUser);
 
 // protected route:
 userRouter.route('/logout').post(verifyJWT, logoutUser)
